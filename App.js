0c8f41444db0df0858f1ee22ee5e9536893734bd
elukuro <heruhartantot@gmail.com>
2021-09-28 21:08:12 +0700

    failed config navigation stack

--- App.js
+++ App.js
@@ -8,8 +8,11 @@
 
 import React, {useState} from 'react';
 import type {Node} from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import SafeAreaView from 'react-native-safe-area-view';
+
 import {
-  SafeAreaView,
   ScrollView,
   StatusBar,
   StyleSheet,
@@ -20,7 +23,11 @@ import {
   TextInput,
   Button,
   FlatList,
+  TouchableHighlight,
+  TouchableWithoutFeedback,
 } from 'react-native';
+
+const Stack = createNativeStackNavigator();
 
 const App: () => Node = () => {
   const isDarkMode = useColorScheme() === 'dark';
@@ -34,16 +41,18 @@ const App: () => Node = () => {
     const [isHungry, setIsHungry] = useState(true);
     return (
       <View>
-        <Image
-          source={{uri: `https://reactnative.dev/docs/assets/${catImage}`}}
-          style={{
-            width: 'auto',
-            height: 300,
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-          }}
-        />
+        <TouchableWithoutFeedback onPress={() => setIsHungry(!isHungry)}>
+          <Image
+            source={{uri: `https://reactnative.dev/docs/assets/${catImage}`}}
+            style={{
+              width: 'auto',
+              height: 300,
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center',
+            }}
+          />
+        </TouchableWithoutFeedback>
         <Button
           onPress={() => {
             setIsHungry(!isHungry);
@@ -52,6 +61,56 @@ const App: () => Node = () => {
         />
         <Text>cat name is {isHungry ? 'Iam so hungry' : 'Iam not hungry'}</Text>
       </View>
+    );
+  };
+  const Profile = ({navigation, route}) => {
+    return <Text>This is {route.params.name}'s profile</Text>;
+  };
+  const Home = ({navigation}) => {
+    return (
+      <ScrollView>
+        <View>
+          <Button
+            title="Go to Jane's profile"
+            onPress={() => navigation.navigate('Profile', {name: 'Jane'})}
+          />
+          <Text>some text here</Text>
+          <View>
+            <Text>Some more text</Text>
+          </View>
+        </View>
+        <Cat name="nero" />
+        <Cat name="toru" />
+        <TextInput
+          style={{height: 40, borderColor: 'gray', borderWidth: 1}}
+          defaultValue=""
+          placeholder="type here to translate"
+          onChangeText={text => setText(text)}
+        />
+        <Text>{text}</Text>
+        <View style={style.container}>
+          <FlatList
+            data={[
+              {key: 'Devin'},
+              {key: 'Booker'},
+              {key: 'Lamelo'},
+              {key: 'Ball'},
+              {key: 'Deangello'},
+              {key: 'russel'},
+              {key: 'wesbrok'},
+            ]}
+            renderItem={({item}) => <Text style={style.item}>{item.key}</Text>}
+          />
+        </View>
+      </ScrollView>
+    );
+  };
+  const HomeScreen = ({navigation}) => {
+    return (
+      <Button
+        title="Go to Jane's profile"
+        onPress={() => navigation.navigate('Profile', {name: 'Jane'})}
+      />
     );
   };
 
@@ -68,37 +127,16 @@ const App: () => Node = () => {
     },
   });
   return (
-    <ScrollView>
-      <View>
-        <Text>some text here</Text>
-        <View>
-          <Text>Some more text</Text>
-        </View>
-      </View>
-      <Cat name="nero" />
-      <Cat name="toru" />
-      <TextInput
-        style={{height: 40, borderColor: 'gray', borderWidth: 1}}
-        defaultValue=""
-        placeholder="type here to translate"
-        onChangeText={text => setText(text)}
-      />
-      <Text>{text}</Text>
-      <View style={style.container}>
-        <FlatList
-          data={[
-            {key: 'Devin'},
-            {key: 'Booker'},
-            {key: 'Lamelo'},
-            {key: 'Ball'},
-            {key: 'Deangello'},
-            {key: 'russel'},
-            {key: 'wesbrok'},
-          ]}
-          renderItem={({item}) => <Text style={style.item}>{item.key}</Text>}
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{title: 'Welcome'}}
         />
-      </View>
-    </ScrollView>
+        <Stack.Screen name="Profile" component={Profile} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 
