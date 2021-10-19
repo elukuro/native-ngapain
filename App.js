53aa159ded43fa37a9d687e021bfe659e5192cfe
elukuro <heruhartantot@gmail.com>
2021-10-19 08:14:42 +0700

    surat list

--- App.js
+++ App.js
@@ -6,48 +6,31 @@
  * @flow strict-local
  */
 
-import React, {useState} from 'react';
+import React from 'react';
 import type {Node} from 'react';
-import {NavigationContainer} from '@react-navigation/native';
+import {
+  NavigationContainer,
+  DefaultTheme,
+  DarkTheme,
+} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import SafeAreaView from 'react-native-safe-area-view';
 import UiHome from './container/Home.js';
 
-import {
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-  Image,
-  TextInput,
-  Button,
-  FlatList,
-  TouchableHighlight,
-  TouchableWithoutFeedback,
-} from 'react-native';
-
 const Stack = createNativeStackNavigator();
-
+const MyTheme = {
+  dark: false,
+  colors: {
+    primary: 'rgb(255, 45, 85)',
+    background: 'rgb(242, 242, 242)',
+    card: 'rgb(255, 255, 255)',
+    text: 'rgb(28, 28, 30)',
+    border: 'rgb(199, 199, 204)',
+    notification: 'rgb(255, 69, 58)',
+  },
+};
 const App: () => Node = () => {
-  const Profile = ({navigation, route}) => {
-    return <Text>This is {route.params.name}'s profile</Text>;
-  };
-  const [text, setText] = useState('');
-  const style = StyleSheet.create({
-    container: {
-      flex: 1,
-      paddingTop: 22,
-    },
-    item: {
-      padding: 10,
-      fontSize: 8,
-      height: 44,
-    },
-  });
   return (
-    <NavigationContainer>
+    <NavigationContainer theme={MyTheme}>
       <Stack.Navigator>
         <Stack.Screen name="Home" component={UiHome} />
       </Stack.Navigator>
--- container/Home.js
+++ container/Home.js
@@ -2,32 +2,33 @@ import React, {useState, useEffect, useCallback} from 'react';
 import API from './../api/index.js';
 
 import {
-  ScrollView,
-  StatusBar,
   StyleSheet,
   Text,
-  useColorScheme,
   View,
   Image,
   TextInput,
   Button,
   FlatList,
+  SafeAreaView,
   TouchableHighlight,
   TouchableWithoutFeedback,
+  TouchableOpacity,
 } from 'react-native';
 
 function Home() {
   const [surahList, setSurahList] = useState();
-  const [renderSurah, setRenderSurah] = useState();
+  const [selectedSurah, setSelectedSurah] = useState();
   useEffect(() => {
     API.getSurah().then(function (result) {
       setSurahList(result.data);
     });
-    API.getDetail({id: '1', ayat: '1'}).then(function (result) {
-      setRenderSurah(result);
-    });
   }, []);
   const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#374151',
+      padding: 8,
+    },
     baseText: {
       fontFamily: 'fonts',
     },
@@ -35,15 +36,38 @@ function Home() {
       fontSize: 20,
       fontWeight: 'bold',
     },
+    item: {
+      padding: 10,
+      color: '#fff',
+    },
   });
 
-  if (renderSurah) {
+  const selectSurah = id => {
+    setSelectedSurah(id);
+  };
+  const Item = ({item}) => {
     return (
-      <ScrollView>
-        <View>
-          <Text style={styles.baseText}>{renderSurah[0].aya_text}</Text>
-        </View>
-      </ScrollView>
+      <View>
+        <TouchableOpacity
+          style={styles.item}
+          onPress={() => selectSurah(item.id)}>
+          <Text>{item.surat_name}</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  };
+
+  if (surahList) {
+    return (
+      <SafeAreaView style={styles.container}>
+        {/* <Text>{JSON.stringify(surahList)}</Text> */}
+        <Text>{selectedSurah}</Text>
+        <FlatList
+          data={surahList}
+          renderItem={Item}
+          keyExtractor={item => item.id}
+        />
+      </SafeAreaView>
     );
   } else {
     return <Text>loading...</Text>;
